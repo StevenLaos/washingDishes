bcd85af81a4705d2db71434577d6d3e9f498012f
Steven Laos <stevenlaos1996@gmail.com>
2017-06-16 16:25:23 +0300

    Made java calculation file to calculate dishwashing and in html started with select function for worker.

---
+++ src/main/java/com/example/calculations/Main.java
@@ -0,0 +1,26 @@
+package com.example.calculations;
+
+/**
+ * Created by User on 6/16/2017.
+ */
+public class Main {
+}
+//    function washing(dishes, liquid, worker){
+//            const dishes = document.getElementById("dishes").value
+//            const liquid = document.getElementById("liquid").value
+//            const washer = document.getElementById("dishWasher").value
+//        if(typeof dishes === "number" && typeof liquid === "number"){
+//            if(dishes > liquid){
+//                    const dishRemaining = dishes - (washer * liquid)
+//                document.getElemenyById("anwser").innerHTML = "Washer used all liquid and remained " + dishRemaining + " dishes."
+//            }else if(dishes < liquid){
+//                    const liquidRemaining = liquid - (washer * dishes)
+//                document.getElementById("anwser").innerHTML = "Washer washed all the dishes and remained " + liquidRemaining + " ml of liquid."
+//            }
+//            else{
+//
+//            }
+//        } else{
+//            return `You didnt enter number or you miss clicked and typed letter or symbol.`
+//        }
+//    }
