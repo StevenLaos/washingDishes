27c0557cee92c7b4512e13600487e5c63c30012b
Steven Laos <stevenlaos1996@gmail.com>
2017-06-27 15:52:02 +0300

    Fixing mistakes and trying still to send data to backend.

--- src/main/webapp/GetWorkerInfo.js
+++ src/main/webapp/GetWorkerInfo.js
@@ -2,29 +2,30 @@
  * Created by User on 6/15/2017.
  */
 function getAllWorkers() {
-    const ul = document.getElementById('dishWasher')
+    const listOfWorkers = document.getElementById('dishWasher')
     const url = 'http://localhost:9000/rest/worker'
     fetch(url, {
         method: 'get',
         headers: {
             'Content-Type': 'application/json'
         },
-    }).then((response) => response.json()
-    ).then(function (data) {
-        let workerList = ""
-        for (let i = 0; i < data.length; i++) {
-            workerList += `<select><option>${data[i].firstName} ${data[i].lastName}</option></select>`
-        }
-        return ul.innerHTML = workerList
     })
+        .then((response) => response.json())
+        .then(function (data) {
+            let workerList = ""
+            for (let i = 0; i < data.length; i++) {
+                workerList += `<select><option>${data[i].firstName} ${data[i].lastName}</option></select>`
+            }
+            listOfWorkers.innerHTML = workerList
+        })
 }
-getAllWorkers()
+window.addEventListener('load', getAllWorkers)
 
 function rdyToWash() {
-    const dishAmount = document.getElementById('dishes')
-    const liquidAmount = document.getElementById('liquid')
-    const workerWork = document.getElementById('dishWasher')
-    const url = 'localhost:9000/rest/washer'      //Aadress peaks õige olema.
+    const dishAmount = document.getElementById('dishes').value
+    const liquidAmount = document.getElementById('liquid').value
+    const workerWork = document.getElementById('dishWasher').value
+    const url = 'http://localhost:9000/rest/washer'
     fetch(url, {
         method: 'post',
         headers: {
@@ -35,25 +36,27 @@ function rdyToWash() {
             liquidAmount,
             workerWork,
         })
-    }).then((response) => response.json()
-    ).then(function (data) {
-        console.log(data)
     })
-
+        .then((response) => response.json())
+        .then(function (data) {
+            console.log(data)
+        })
 }
-rdyToWash()
 
-function answerSentence() {
-    const ul = document.getElementById("answer")
-    const url = ''
-    fetch(url, {
-        method: 'get',
-        headers: {
-            'Content-type': 'application/json'
-        },
-    }).then((response) => response.json()
-    ).then(function (answer) {
 
-        return ul.innerHTML = `testTEST tekst`; //Mõelda välja funktsioon ja sisestada siis vastus
-    })
-}
+/*
+ function answerSentence() {
+ const text = document.getElementById("answer")
+ const url = ''
+ fetch(url, {
+ method: 'get',
+ headers: {
+ 'Content-type': 'application/json'
+ },
+ }).then((response) => response.json()
+ ).then(function (answer) {
+ //Mõelda välja funktsioon ja sisestada siis vastus
+ return text.innerHTML = `testTEST tekst`; //textile parem nimi ja = märgi taha funktsiooni vastsus
+ })
+ }
+ */
